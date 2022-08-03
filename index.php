8bffc3fa5a164a0d4af7073472fbb65487e91a87
10994015 <a0938599191@gmail.com>
2022-08-03 14:00:41 +0800

    0803

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=1.0, maximum-scale=3.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DYM</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+    <header id="header">
+        <a href="./" class="logo"><img src="./images/logo.png" class="logo"></a>
+        <div class="left">
+            <a href="./web/login.php" class="login">會員登入</a>
+            <a href="./web/register.php" class="register">免費註冊</a>
+            <select name="" id="">
+                <option value=""> 
+                    繁體中文
+                </option>
+            </select>
+        </div>
+    </header>
+    <nav id="nav">
+        <a href="javascript:;">遊戲大廳</a>
+        <a href="javascript:;">真人視訊</a>
+        <a href="javascript:;">飛機競速</a>
+        <a href="javascript:;">體育賽事</a>
+        <a href="javascript:;">優惠活動</a>
+    </nav>
+</body>
+</html>
